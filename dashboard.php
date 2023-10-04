89bd154a7818d235eabb91c3b5abde93c3409f4d
Prayoga K. I <113886602+Pk2413@users.noreply.github.com>
2023-10-04 13:23:56 +0700

    update
    dashboard index login

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -19,7 +19,7 @@
                 <img src="gambar/Logo-Kabupaten-Nganjuk-Warna-removebg-preview 2.png" height="50">
                 Pesudukuh</a>
             <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 justify-content-end" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <!-- <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
