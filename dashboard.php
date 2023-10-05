445e050e4e101f9fc4122418b0237a034deb7b87
Prayoga K. I <113886602+Pk2413@users.noreply.github.com>
2023-10-05 17:39:04 +0700

    landingpages
    landingpages

--- dashboard.php
+++ dashboard.php
@@ -19,7 +19,7 @@
                 <img src="gambar/Logo-Kabupaten-Nganjuk-Warna-removebg-preview 2.png" height="50">
                 Pesudukuh</a>
             <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 justify-content-end" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 jus" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <!-- <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
--- dashboard2.php
+++ dashboard2.php
@@ -1,18 +1,18 @@
 <?php
-include "koneksi.php";
-include "dashboard.html";
+// include "koneksi.php";
+// include "dashboard.html";
 
-$no = 1;
-$ambildata = mysqli_query($conn, "Select * from akun_admin");
-while ($tampil = mysqli_fetch_array($ambildata)){
-    echo"
-    <tr>
-        <td>$no</th>
-        <td>$tampil[username]</th>
-        <td>$tampil[password]</th>
-        <td>$tampil[email]</th>
-    </tr>";
-    $no++;
-}
+// $no = 1;
+// $ambildata = mysqli_query($conn, "Select * from akun_admin");
+// while ($tampil = mysqli_fetch_array($ambildata)){
+//     echo"
+//     <tr>
+//         <td>$no</th>
+//         <td>$tampil[username]</th>
+//         <td>$tampil[password]</th>
+//         <td>$tampil[email]</th>
+//     </tr>";
+//     $no++;
+// }
 
 ?>
